98a7ea395be3713bc9be2270d057c39bffcd203d
Valentin Duffet <vavadu07@gmail.com>
2021-08-03 14:29:50 +0200

    Check saisie de l'input

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
 
-  const [tache, setTache] = useState();
+  const [tache, setTache] = useState(null);
   const [listeTache, setListeTache] = useState([])
 
   let clickAdd = () => {
-    setListeTache([...listeTache, tache]);
-    setTache(null);
+    if (tache !== null) {
+      setListeTache([...listeTache, tache]);
+      setTache(null);
 
-    // Close keyboard
-    Keyboard.dismiss();
+      // Close keyboard
+      Keyboard.dismiss();
+    } else {
+      alert("Veuillez saisir du texte !");
+    }
+    
   }
 
   let reverseDataDelete = index => {
@@ -28,17 +33,19 @@ export default function App() {
         <Text style={{fontWeight: "bold", fontSize: 20, color: "white"}}>Tàche à effectuer aujourd'hui !</Text>
       </View>
 
-      <View style={{margin: 20}}>
-        {listeTache.length > 0 ? listeTache.map((tache, i) => {
-          return(
-            <Task key={i} titreTache={tache} position={i} clickDelete={reverseDataDelete}/>
-          )
-        }) : <Text style={{fontSize: 18, textAlign: "center", paddingVertical: "50%"}}>Aucune tache</Text>}
+      <View style={{margin: 20, height: "75%"}}>
+        <ScrollView>
+          {listeTache.length > 0 ? listeTache.map((tache, i) => {
+            return(
+              <Task key={i} titreTache={tache} position={i} clickDelete={reverseDataDelete}/>
+            )
+          }) : <Text style={{fontSize: 18, textAlign: "center", paddingVertical: "50%"}}>Aucune tache</Text>}
+        </ScrollView>
       </View>
 
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={{position: "absolute", bottom: 40, width: "100%", flexDirection: "row", justifyContent: "space-around", alignItems: "center"}}
+        style={{position: "absolute", bottom: 20, width: "100%", flexDirection: "row", justifyContent: "space-around", alignItems: "center"}}
       >
         <TextInput 
           placeholder="Nouvelle tache..." 
